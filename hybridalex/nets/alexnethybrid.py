9a243df845711d06319ecbc64da8a73cf77d09df
aychdong <aychdong@sjtu.edu.cn>
2017-11-11 15:11:48 -0500

    Update alexnethybrid.py

--- hybridalex/nets/alexnethybrid.py
+++ hybridalex/nets/alexnethybrid.py
@@ -57,19 +57,24 @@ def original(images, labels, num_classes, total_num_examples, devices=None, is_t
         train_op = train(total_loss, global_step, total_num_examples)
     return net, logits, total_loss, train_op, global_step
 
-
 def ndev_data(images, labels, num_classes, total_num_examples, devices, is_train=True):
-    """Build inference, data parallelism"""
-    # use the last device in list as variable device
-    devices = devices[:]
-    builder = ModelBuilder(devices.pop())
+    # Your code starts here...
+    devices_list = devices[:]
 
+    # Use first device as the variable device. 
+    builder = ModelBuilder(devices_list[0])
+    devices_list = devices_list[1:]
+
+    # If is_not_train, return alexnet_eval()
     if not is_train:
-        with tf.variable_scope('model'):
-            prob = alexnet_inference(builder, images, labels, num_classes)[0]
-        return alexnet_eval(prob, labels)
+        net, _, _ = alexnet_inference(builder, images, labels, num_classes)
+        return alexnet_eval(net, labels)
 
-    # configure optimizer
+    #Create global steps on the parameter server node. You can use the same method that the single-machine program uses to perform this step.
+    with tf.device(builder.variable_device()):
+        global_step = builder.ensure_global_step()
+
+    #Configure the optimizer using the global step created and the total examples number user passed into ndev_data.
     def configure_optimizer(global_step, total_num_steps):
         """Return a configured optimizer"""
         def exp_decay(start, tgtFactor, num_stairs):
@@ -88,39 +93,33 @@ def ndev_data(images, labels, num_classes, total_num_examples, devices, is_train
             'weights': lparam(exp_decay(0.001, 250, 4), 0.9),
             'biases': lparam(exp_decay(0.002, 10, 2), 0.9),
         })
-
-    with tf.device(builder.variable_device()):
-        global_step = builder.ensure_global_step()
     opt = configure_optimizer(global_step, total_num_examples)
 
-    # construct each replica
-    replica_grads = []
+    #Construct graph replica by splitting the original tensors into sub tensors. You need to create replica for both images and labels. Take a look at how tf.split works.
+    grads_list = []
     with tf.device(builder.variable_device()):
-        image_slices = tf.split(images, len(devices), 0)
-        label_slices = tf.split(labels, len(devices), 0)
-    with tf.variable_scope('model') as vsp:
-        # we only want scope for variables but not operations
+        image_list = tf.split(images, len(devices_list), 0)
+        label_list = tf.split(labels, len(devices_list), 0)
+    with tf.variable_scope('model') as var_scope:
         with tf.name_scope(''):
-            for idx in range(len(devices)):
-                dev = devices[idx]
-                with tf.name_scope('tower_{}'.format(idx)) as scope:
+            for i in range(len(devices)):
+                dev = devices[i]
+                with tf.name_scope('tower_{}'.format(i)) as name_scope:
                     with tf.device(dev):
-                        prob, logits, total_loss = alexnet_inference(builder, image_slices[idx],
-                                                                     label_slices[idx], num_classes,
-                                                                     scope)
+                        #building up network layers by calling alexnet_inference/vgg_inference using the sub tensors you created in Step 3
+                        net, logits, total_loss = alexnet_inference(builder, image_slices[i],label_slices[i], num_classes,name_scope)
                         # calculate gradients for batch in this replica
-                        grads = opt.compute_gradients(total_loss)
+                        tmp_grads = opt.compute_gradients(total_loss)
 
-                replica_grads.append(grads)
+                grads_list.append(tmp_grads)
                 # reuse variable for next replica
-                vsp.reuse_variables()
+                var_scope.reuse_variables()
 
-    # average gradients across replica
+    #On the parameter server node, calculate the average accross the gradients you collect. You should call average_gradents method definded in the ModuleBuilder class for this step. This method will transfer gradients from the worker nodes to the parameter server.
     with tf.device(builder.variable_device()):
-        grads = builder.average_gradients(replica_grads)
-        apply_grads_op = opt.apply_gradients(grads, global_step=global_step)
+        gradients = builder.average_gradients(grads_list)
+        apply_gradient_op = opt.apply_gradients(gradients, global_step=global_step)
+        train_op = tf.group(apply_gradient_op, name='train')
 
-        train_op = tf.group(apply_grads_op, name='train')
-
-    # simply return prob, logits, total_loss from the last replica for simple evaluation
-    return prob, logits, total_loss, train_op, global_step
+    #Finally, return the same parameters as the single-machine version code does.
+    return net, logits, total_loss, train_op, global_step
